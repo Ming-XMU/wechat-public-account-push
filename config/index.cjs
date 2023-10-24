20098b1b118f28e4bb4b84e51d6ca9ffca5ef109
Yiming Zhang <84370356+Ming-XMU@users.noreply.github.com>
2023-10-24 21:05:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx34f8a7de28948388',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'wx34f8a7de28948388',
 
   PROVINCE: '北京',
   CITY: '北京',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '3CswfTOIfsPywEVRxrG9sEDr9p1Y38IVknXISlw0uhg',
+      id: 'wx34f8a7de28948388',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '3CswfTOIfsPywEVRxrG9sEDr9p1Y38IVknXISlw0uhg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
