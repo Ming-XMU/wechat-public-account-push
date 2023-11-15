655118043bc417d2985d39b2a28e8710a84967b8
Yiming Zhang <84370356+Ming-XMU@users.noreply.github.com>
2023-11-15 15:02:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMni76DNCoOjspvNSilc6DW4NoNk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'C7Zr0-pQ6NWh1RHQK3Aa_FmIpmUDMu0wHLhnKRmF10E',
+      useTemplateId: 'VjYncz8Qrx6JK4dDow9GSjGy4Q9A6EMeZcfu3cjo7Ec',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-16',
       festivals: [
